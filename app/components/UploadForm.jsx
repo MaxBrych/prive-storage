1aba957067341c7c5f9134aab6889e81e8dc0957
MaxBrych <max.brych03@gmail.com>
2023-10-27 07:44:41 +0200

    installed tiptap rich text editor

--- app/components/UploadForm.jsx
+++ app/components/UploadForm.jsx
@@ -2,8 +2,13 @@
 // app/components/NFTForm.tsx
 import React, { useState } from "react";
 import { uploadMetadata } from "../utils/uploadMetadata";
+import TipTap from "./Editor/TipTap";
+import Tiptap from "./Editor/TipTap";
+import { useRef } from "react";
 
 export const UploadForm = () => {
+  const editorRef = (useRef < { getHTML: () => string }) | (null > null);
+
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [image, setImage] = useState(null);
@@ -33,11 +38,13 @@ export const UploadForm = () => {
           placeholder="Description"
           className="w-full p-2 border rounded"
         />
+        <TipTap ref={editorRef} />
         <input
           type="file"
           onChange={(e) => setImage(e.target.files ? e.target.files[0] : null)}
           className="w-full p-2 border rounded"
         />
+
         <button
           type="submit"
           className="px-4 py-2 text-white bg-blue-500 rounded"
