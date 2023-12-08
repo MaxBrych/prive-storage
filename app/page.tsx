01bb2bebd61e33126f9f9651fa842af68c71b9ca
MaxBrych <max.brych03@gmail.com>
2023-12-08 05:40:31 +0100

    max width

--- app/page.tsx
+++ app/page.tsx
@@ -6,45 +6,47 @@ import { ArrowUpRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 bg-white md:flex-row">
-      {/* Left Column */}
-      <div className="w-full px-4 py-32 mx-auto md:px-8 md:max-w-1/2 sm:py-48 lg:py-56">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-start">
-          <div className="relative px-3 py-1 text-sm leading-6 text-gray-600 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            Store files with provanance on Iry.{" "}
-            <a
-              href="https://irys.xyz/"
-              className="font-semibold text-indigo-600"
+    <div className="justify-center w-full min-h-screen">
+      <div className="flex flex-col max-w-5xl gap-4 bg-white md:flex-row">
+        {/* Left Column */}
+        <div className="w-full px-4 py-32 mx-auto md:px-8 md:max-w-1/2 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-start">
+            <div className="relative px-3 py-1 text-sm leading-6 text-gray-600 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Store files with provanance on Iry.{" "}
+              <a
+                href="https://irys.xyz/"
+                className="font-semibold text-blue-500"
+              >
+                <span className="absolute inset-0" aria-hidden="true" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div className="text-start">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Permanent File Storage onchain
+            </h1>
+            <p className="mt-6 mb-4 text-lg leading-8 text-gray-600">
+              Store your images on arweave with ease and ensure its provenance
+              and permanent avalability
+            </p>
+            <Link
+              href="/dashboard"
+              type="button"
+              className="-m-2.5 mt-2 inline-flex items-center gap-2 rounded-lg px-3 bg-zinc-100 justify-center text-sm py-2.5 text-gray-700"
             >
-              <span className="absolute inset-0" aria-hidden="true" />
-              Read more <span aria-hidden="true">&rarr;</span>
-            </a>
+              Dashboard
+              <ArrowUpRight className="w-4 h-4" aria-hidden="true" />
+            </Link>
           </div>
         </div>
-        <div className="text-start">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Permanent File Storage onchain
-          </h1>
-          <p className="mt-6 mb-4 text-lg leading-8 text-gray-600">
-            Store your images on arweave with ease and ensure its provenance and
-            permanent avalability
-          </p>
-          <Link
-            href="/dashboard"
-            type="button"
-            className="-m-2.5 mt-2 inline-flex items-center gap-2 rounded-lg px-3 bg-zinc-100 justify-center text-sm py-2.5 text-gray-700"
-          >
-            Dashboard
-            <ArrowUpRight className="w-4 h-4" aria-hidden="true" />
-          </Link>
-        </div>
-      </div>
 
-      {/* Right Column*/}
-      <div className="flex flex-col items-center justify-center w-full md:min-h-screen md:max-w-1/2">
-        {/*  <UploadFiles /> */}
-        <div className="">
-          <UploadTabs />
+        {/* Right Column*/}
+        <div className="flex flex-col items-center justify-center w-full md:min-h-screen md:max-w-1/2">
+          {/*  <UploadFiles /> */}
+          <div className="">
+            <UploadTabs />
+          </div>
         </div>
       </div>
     </div>
